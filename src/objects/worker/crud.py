17c1cbd55727882c442deb6da4f7a510f47f634e
AndreyChuyan <oksigen077777@gmail.com>
2024-09-05 18:14:36 +0300

    настроена сортировка пользователей по подразделению у админа

--- src/objects/worker/crud.py
+++ src/objects/worker/crud.py
@@ -28,7 +28,7 @@ class CRUDWorker(CRUDBase):
     @classmethod
     async def get_all_worker_sorted(cls, session: AsyncSession) -> list[model]:
         """Получение всех компьютеров в сортировке"""
-        order_by = [cls.model.fio]
+        order_by = [cls.model.department]
         data = await cls.get_all(session, order_by=order_by)
         # log.debug(f'Debug --- get_all_arm_sorted data={data}')
         return data
